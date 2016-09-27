8c35d595a523d0154f0f59909b3fc5e555ec1c0e
Ricky Y. Lee <rickylee0914@gmail.com>
2016-09-26 18:45:12 -0700

    On working for integrating with new sampler classes.

--- src/world/ViewPlane.java
+++ src/world/ViewPlane.java
@@ -1,17 +1,30 @@
 package world;
 
+import samplers.*;
+
 /**
  * @author Yuanqi Li
- * @version 0.2
+ * @version 0.5
  */
 public class ViewPlane {
 
-    private int    hres;
-    private int    vres;
-    private double pixelSize;
-    private double gamma;
-    private double invGamma;
-    private int    numSamples;
+/*--------------------------------------------------------------*\
+ *  Fields
+\*--------------------------------------------------------------*/
+
+    private int     hres;
+    private int     vres;
+    private double  pixelSize;
+
+    private double  gamma;
+    private double  invGamma;
+
+    private int     numSamples;
+    private Sampler sampler;
+
+/*--------------------------------------------------------------*\
+ *  Constructors
+\*--------------------------------------------------------------*/
 
     public ViewPlane(int hres, int vres, double pixelSize, double gamma, int numSamples) {
         this.hres = hres;
@@ -19,7 +32,12 @@ public class ViewPlane {
         this.pixelSize = pixelSize;
         this.gamma = gamma;
         this.invGamma = 1 / gamma;
+        this.numSamples = numSamples;
     }
+
+/*--------------------------------------------------------------*\
+ *  Getters and setters
+\*--------------------------------------------------------------*/
 
     public int getHres() {
         return hres;
@@ -51,13 +69,38 @@ public class ViewPlane {
 
     public void setGamma(float gamma) {
         this.gamma = gamma;
+        this.invGamma = 1 / gamma;
     }
 
     public double getInvGamma() {
         return invGamma;
     }
 
-    public void setInvGamma(float invGamma) {
-        this.invGamma = invGamma;
+    public int getNumSamples() {
+        return numSamples;
+    }
+
+    /**
+     * Set the number of sample points. Note this method specifies multi-jittered sampling
+     * by default, and when the number of samples is one, it specifies regular sampling,
+     * which puts one sample point in the center of each pixel. This method will be used
+     * more frequently than <code>setSampler</code>.
+     * @param n number of sample points
+     */
+    void setNumSamples(int n) {
+        numSamples = n;
+        if (numSamples > 1)
+            sampler = new MultiJittered(numSamples);
+        else
+            sampler = new Regular(1);
+    }
+
+    public Sampler getSampler() {
+        return sampler;
+    }
+
+    void setSampler(Sampler sp) {
+        numSamples = sp.getNumSamples();
+        sampler = sp;
     }
 }
--- src/world/World.java
+++ src/world/World.java
@@ -13,19 +13,27 @@ import java.util.List;
  */
 public class World {
 
+/*--------------------------------------------------------------*\
+ *  Fields
+\*--------------------------------------------------------------*/
+
     private ViewPlane             vp;
     private RgbColor              backgroundColor = RgbColor.BLACK;
     private Tracer                tracer  = new MultipleObjects(this);
     private List<GeometricObject> objects = new ArrayList<>();
     private Sphere                sphere;
 
+/*--------------------------------------------------------------*\
+ *  Constructors
+\*--------------------------------------------------------------*/
+
     public World(ViewPlane vp) {
         this.vp = vp;
     }
 
-    public void addObject(GeometricObject object) {
-        objects.add(object);
-    }
+/*--------------------------------------------------------------*\
+ *  Public methods
+\*--------------------------------------------------------------*/
 
     public ShadeRec hitBareBonesObjects(final Ray ray) {
         ShadeRec sr = new ShadeRec(this);
@@ -60,10 +68,49 @@ public class World {
         }
     }
 
-    public void openWindow(int hres, int vres) {
+    public void renderSceneWithSampler() {
+        RgbColor pixelColor;
+        Ray      ray = new Ray(new Point3D(0, 0, 0), new Vector3D(0, 0, -1));
+        double   zw = 100.0;
+        Point2D  sp;                    // sample point in [0,1]x[0.1]
+        Point2D  pp = new Point2D();    // sample point on a pixel
 
+        openWindow(vp.getHres(), vp.getVres());
+
+        for (int r = 0; r < vp.getVres(); r++)              // up
+            for (int c = 0; c <= vp.getHres(); c++) {       // across
+                pixelColor = RgbColor.BLACK;
+
+                for (int j = 0; j < vp.getNumSamples(); j++) {
+                    sp = vp.getSampler().sampleUnitSquare();
+                    pp.x = vp.getPixelSize() * (c - 0.5 * vp.getHres() + sp.x);
+                    pp.y = vp.getPixelSize() * (r - 0.5 * vp.getVres() + sp.y);
+                    ray.setOrigin(new Point3D(pp.x, pp.y, zw));
+                    pixelColor.add(tracer.trace(ray));
+                }
+                pixelColor = pixelColor.div(vp.getNumSamples());
+                displayPixel(r, c, pixelColor);
+            }
     }
 
+    public void addObject(GeometricObject object) {
+        objects.add(object);
+    }
+
+    /**
+     * Opens a new window for drawing graphs.
+     * @param hres h resolution
+     * @param vres v resolution
+     */
+    public void openWindow(int hres, int vres) {
+    }
+
+    /**
+     * Displays pixels on somewhere.
+     * @param row        the row of the pixel
+     * @param column     the column of the pixel
+     * @param pixelColor the color of the pixel to be drawn
+     */
     public void displayPixel(int row, int column, RgbColor pixelColor) {
         if (pixelColor.equals(RgbColor.RED)) {
             System.out.print(Console.ANSI_RED + "**" + Console.ANSI_RESET);
@@ -75,22 +122,47 @@ public class World {
             System.out.print("  ");
     }
 
+/*--------------------------------------------------------------*\
+ *  Getters and setters
+\*--------------------------------------------------------------*/
+
+    public ViewPlane getVp() {
+        return vp;
+    }
+
+    public void setVp(ViewPlane vp) {
+        this.vp = vp;
+    }
+
     public RgbColor getBackgroundColor() {
         return backgroundColor;
     }
+
     public void setBackgroundColor(RgbColor backgroundColor) {
         this.backgroundColor = backgroundColor;
     }
-    public Sphere getSphere() {
-        return sphere;
+
+    public Tracer getTracer() {
+        return tracer;
     }
-    public void setSphere(Sphere sphere) {
-        this.sphere = sphere;
+
+    public void setTracer(Tracer tracer) {
+        this.tracer = tracer;
     }
+
     public List<GeometricObject> getObjects() {
         return objects;
     }
+
     public void setObjects(List<GeometricObject> objects) {
         this.objects = objects;
+    }
+
+    public Sphere getSphere() {
+        return sphere;
+    }
+
+    public void setSphere(Sphere sphere) {
+        this.sphere = sphere;
     }
 }
