fd3de3f448fae95e2771379b98292e180e52f082
Ricky Yuanqi Li <yuanqili@umail.ucsb.edu>
2016-10-15 12:12:11 -0700

    Now matte material works

--- src/materials/Matte.java
+++ src/materials/Matte.java
@@ -6,52 +6,31 @@ import utilities.RgbColor;
 import utilities.ShadeRec;
 import utilities.Vector3D;
 
-/**
- * @author Yuanqi Li
- */
 public class Matte extends Material {
 
     private Lambertian ambientBRDF;
     private Lambertian diffuseBRDF;
 
-    public Matte() {
+    public Matte(double ambientKd, double diffuseKd, RgbColor color) {
         super();
-//        this.ambientBRDF = new Lambertian();
-//        this.diffuseBRDF = new Lambertian();
-    }
-
-    public void setKa(double k) {
-        ambientBRDF.setDiffuseReflectionCoefficient(k);
-    }
-
-    public void setKd(double k) {
-        diffuseBRDF.setDiffuseReflectionCoefficient(k);
-    }
-
-    public void setCd(RgbColor k) {
-        ambientBRDF.setDiffuseColor(k);
-        diffuseBRDF.setDiffuseColor(k);
-    }
-
-    public void setCd(double r, double g, double b) {
-        ambientBRDF.setDiffuseColor(new RgbColor(r, g, b));
-        diffuseBRDF.setDiffuseColor(new RgbColor(r, g, b));
+        this.ambientBRDF = new Lambertian(ambientKd, color);
+        this.diffuseBRDF = new Lambertian(diffuseKd, color);
     }
 
     @Override
     public RgbColor shade(ShadeRec sr) {
 
         Vector3D wo = sr.ray.getDirection().neg();
-        RgbColor L  = ambientBRDF.rho(sr, wo).mul(sr.world.getAmbient().irradiance(sr));
-        int      numLights = sr.world.getLights().size();
+        RgbColor L = ambientBRDF.rho(sr, wo).mul(sr.world.getAmbient().irradiance(sr));
 
         for (Light l : sr.world.getLights()) {
             Vector3D wi = l.getDirection(sr);
-            double ndotwi = sr.normal.mul(wi);
-            if (ndotwi > 0.0)
-                L = L.add(diffuseBRDF.f(sr, wo, wi).mul(l.irradiance(sr)).mul(ndotwi));
+            if (sr.hitNormal.mul(wi) > 0.0)
+                L = L.add(
+                        diffuseBRDF.f(sr, wo, wi)
+                                .mul(l.irradiance(sr))
+                                .mul(sr.hitNormal.mul(wi)));
         }
-
         return L;
     }
 
@@ -63,5 +42,23 @@ public class Matte extends Material {
     @Override
     public RgbColor shadePath(ShadeRec st) {
         return null;
+    }
+
+    public void setKa(double ka) {
+        ambientBRDF.setDiffuseReflectionCoefficient(ka);
+    }
+
+    public void setKd(double kd) {
+        diffuseBRDF.setDiffuseReflectionCoefficient(kd);
+    }
+
+    public void setCd(RgbColor k) {
+        ambientBRDF.setDiffuseColor(k);
+        diffuseBRDF.setDiffuseColor(k);
+    }
+
+    public void setCd(double r, double g, double b) {
+        ambientBRDF.setDiffuseColor(new RgbColor(r, g, b));
+        diffuseBRDF.setDiffuseColor(new RgbColor(r, g, b));
     }
 }
